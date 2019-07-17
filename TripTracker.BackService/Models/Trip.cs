06602dfe5d044160b176c3bf0cc87d4d03933ce6
csenarath <40589106+csenarath@users.noreply.github.com>
2019-07-17 16:25:58 +0530

    Add files via upload

---
+++ TripTracker.BackService/Models/Trip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Trip
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+    }
+}
